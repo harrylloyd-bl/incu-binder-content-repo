eeb13c4d07ae638c8038b3a9c49ab8ef33e77372
harrylloyd-bl <harry.lloyd@bl.uk>
2023-06-02 18:03:47 +0100

    refactor for tracking xml name in export add tests for extract_catalogue_entries and save_raw_txt

--- tests/test_xml_extraction.py
+++ tests/test_xml_extraction.py
@@ -1,5 +1,6 @@
 import src.data.xml_extraction as xmle
 from xml.etree import ElementTree as ET
+import os
 import glob
 import pytest
 from tqdm import tqdm
@@ -10,16 +11,16 @@ tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
 
 @pytest.fixture()
 def xml_roots():
-    xml_roots = []
+    xml_roots = {}
     for xml in glob.glob("tests\\small_*.xml"):
         tree = ET.parse(xml)
         root = tree.getroot()
-        xml_roots.append(root)
-    return xml_roots
+        xml_roots[os.path.basename(xml)[:-4]] = root
+    return {k: xml_roots[k] for k in sorted(xml_roots)}
 
 
 def test_extract_lines(xml_roots):
-    lines = xmle.extract_lines(xml_roots[0])
+    lines = xmle.extract_lines(xml_roots["small_xml_example_1"])
 
     assert len(lines) == 3
     assert lines[0] == "Test line 1"
@@ -35,16 +36,22 @@ def test_extract_lines_text_region_order():
 
 
 def test_extract_lines_for_vol(xml_roots):
-    allLines = xmle.extract_lines_for_vol(xml_roots)
+    all_lines, xml_track_df = xmle.extract_lines_for_vol(xml_roots)
 
-    assert len(allLines) == 6
-    assert allLines[-1] == "Test line 6"
+    assert len(all_lines) == 6
+    assert all_lines[-1] == "Test line 6"
+
+    assert xml_track_df.shape == (6, 2)  # must be same length as all_lines
+    assert xml_track_df['xml'].tolist() == [
+        "small_xml_example_1", "small_xml_example_1", "small_xml_example_1",
+        "small_xml_example_2", "small_xml_example_2", "small_xml_example_2"
+    ]
 
 
 def test_check_line():
-    i_num_check = xmle.check_line("IA. 33")
-    c_num_check = xmle.check_line("C.44")
-    bad_check = xmle.check_line("asdf")
+    i_num_check = xmle.shelfmark_check("IA. 33")
+    c_num_check = xmle.shelfmark_check("C.44")
+    bad_check = xmle.shelfmark_check("asdf")
 
     assert i_num_check
     assert c_num_check
@@ -61,18 +68,32 @@ def test_date_check():
     assert not bad_check
 
 
+# separate so easier to see their values in tests
+fix_in_xml = ET.parse("tests\\title_xml_example.xml")
+fix_root = fix_in_xml.getroot()
+fix_lines = xmle.extract_lines(fix_root)
+fix_titles, fix_title_indices = ["IA. 123TITLE1456", "IA. 789SECONDTITLE1506"], [[0, 1, 2], [6, 7, 8]]# xmle.find_headings(fix_lines)
+
 @pytest.fixture()
-def lines_titles_indices():
-    in_xml = ET.parse("tests\\title_xml_example.xml")
-    root = in_xml.getroot()
-    allLines = xmle.extract_lines(root)
-    titles, allTitleIndices = xmle.find_headings(allLines)
-    return allLines, titles, allTitleIndices
+def lines():
+    return fix_lines
+
+@pytest.fixture()
+def titles():
+    return fix_titles
+
+@pytest.fixture()
+def indices():
+    return fix_title_indices
 
 
-def test_find_headings(xml_roots, lines_titles_indices):
-    all_lines = xmle.extract_lines_for_vol(xml_roots)
-    no_titles, no_all_title_indices = xmle.find_headings(all_lines)
+def test_find_headings(xml_roots, lines, titles, indices):
+    t, i = xmle.find_headings(["asdf"])
+    assert not t
+    assert not i
+
+    no_title_lines, xml_track_df = xmle.extract_lines_for_vol(xml_roots)
+    no_titles, no_all_title_indices = xmle.find_headings(no_title_lines)
     # should both be empty
     assert not no_titles
     assert not no_all_title_indices
@@ -81,15 +102,16 @@ def test_find_headings(xml_roots, lines_titles_indices):
     the xml used to make title_indices contains three IA. numbers that could start titles
     The second IA. (IA. 456) has its title interrupted by IA. 789 before a date is found
     This tests the 
-    `if checkLine(titlePart):
+    `if shelfmark_check(title_part):
     `    break
-    section of findHeadings 
+    section of find_headings 
     """
-    _, titles, all_title_indices = lines_titles_indices
 
-    assert len(titles), len(all_title_indices) == (2, 2)
+    titles, indices = xmle.find_headings(lines)
+
+    assert len(titles), len(indices) == (2, 2)
     assert titles == ["IA. 123TITLE1456", "IA. 789SECONDTITLE1506"]
-    assert all_title_indices == [[0, 1, 2], [6, 7, 8]]
+    assert indices == [[0, 1, 2], [6, 7, 8]]
 
 
 def test_get_i_num_title():
@@ -131,13 +153,11 @@ def test_find_title_ref(capsys):
     assert captured.out == "Unrecognized title format\n"
 
 
-def test_gen_title_refs(lines_titles_indices):
-    all_lines, _, all_title_indices = lines_titles_indices
-    title_refs = xmle.gen_title_refs(all_title_indices, all_lines)
-    # two titles/lists of title indices in lines_titles_indices
-    # genTitleRefs selects [:-1] so only get one out
-    assert len(title_refs) == 1
-    assert title_refs[0] == "IA. 123TI"
+def test_gen_title_refs(lines, indices):
+    title_refs = xmle.gen_title_refs(lines, indices)
+
+    assert len(title_refs) == 2
+    assert title_refs == ["IA. 123TI", "IA. 789SE"]
 
 
 def test_generate_xml():
@@ -147,3 +167,42 @@ def test_generate_xml():
     title, all_title_indices = xmle.find_headings(all_lines)
 
     # out_xml = xmle.generateXML(all_title_indices, all_lines, )
+
+
+def test_extract_catalogue_entries(lines, indices):
+    root = ET.parse("tests\\title_xml_example.xml").getroot()
+    _, xml_track_df = xmle.extract_lines_for_vol({"title_xml_example": root})
+    lines += ["", "IA. 666", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    indices += [[10, 11, 12], [14, 15, 16]]  # need an extra pair due to [:-2] indexing in save_raw_txt
+    title_refs = xmle.gen_title_refs(lines, indices)
+
+    catalogue_entries = xmle.extract_catalogue_entries(lines, indices, title_refs, xml_track_df)
+
+    assert catalogue_entries.shape == (2,2)
+    assert catalogue_entries["labels"].tolist() == ["title_xml_example_IA_123", "title_xml_example_IA_789SE"]
+
+
+def test_save_raw_txt(tmp_path, lines, titles, indices):
+    root = ET.parse("tests\\title_xml_example.xml").getroot()
+    _, xml_track_df = xmle.extract_lines_for_vol({"title_xml_example": root})
+    lines, indices = lines, indices
+    lines += ["", "IA. 666", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    indices += [[10, 11, 12], [14, 15, 16]]  # need an extra pair due to [:-2] indexing in save_raw_txt
+    title_refs = xmle.gen_title_refs(lines, indices)
+    out_path = tmp_path
+
+    xmle.save_raw_txt(lines, indices, title_refs, xml_track_df, out_path)
+
+    filepath_1 = tmp_path.joinpath(f"{xml_track_df.iloc[0, 0]}_IA_789SE.txt")
+    filepath_2 = tmp_path.joinpath(f"{xml_track_df.iloc[0,0]}_IA_666.txt")
+
+    assert len(indices) == 4
+    assert os.path.exists(filepath_1)
+    assert os.path.exists(filepath_2)
+
+    with open(filepath_1) as f:
+        saved_lines = f.readlines()
+
+    assert len(saved_lines) == 5
+    assert saved_lines[0] == "TITLE\n"
+    assert saved_lines[-1] == "TITLE\n"
