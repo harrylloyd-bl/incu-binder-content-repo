a00b4a5cfc4995784edbf7e033b68c2e396730b3
harrylloyd-bl <harry.lloyd@bl.uk>
2023-06-07 11:20:36 +0100

    update find_headings tests to expect title_shelfmarks as output add new cols to extract_catalogue_entries test

--- tests/test_xml_extraction.py
+++ tests/test_xml_extraction.py
@@ -72,15 +72,18 @@ def test_date_check():
 fix_in_xml = ET.parse("tests\\title_xml_example.xml")
 fix_root = fix_in_xml.getroot()
 fix_lines = xmle.extract_lines(fix_root)
-fix_titles, fix_title_indices = ["IA. 123TITLE1456", "IA. 789SECONDTITLE1506"], [[0, 1, 2], [6, 7, 8]]# xmle.find_headings(fix_lines)
+fix_titles, fix_title_indices = ["IA. 123TITLE1456", "IA. 789SECONDTITLE1506"], [[0, 1, 2], [6, 7, 8]]
+
 
 @pytest.fixture()
 def lines():
     return fix_lines
 
+
 @pytest.fixture()
 def titles():
     return fix_titles
+
 
 @pytest.fixture()
 def indices():
@@ -107,10 +110,11 @@ def test_find_headings(xml_roots, lines, titles, indices):
     section of find_headings 
     """
 
-    titles, indices = xmle.find_headings(lines)
+    # lines is produced from title_xml_example.xml rather than the small_xml_examples
+    title_shelfmarks, indices = xmle.find_headings(lines)
 
-    assert len(titles), len(indices) == (2, 2)
-    assert titles == ["IA. 123TITLE1456", "IA. 789SECONDTITLE1506"]
+    assert len(title_shelfmarks), len(indices) == (2, 2)
+    assert title_shelfmarks == ["IA. 123TI", "IA. 789SE"]
     assert indices == [[0, 1, 2], [6, 7, 8]]
 
 
@@ -136,28 +140,28 @@ def test_get_c_num_title():
     assert title_fourstop == "C.31.a.2"
 
 
-def test_find_title_ref(capsys):
+def test_find_title_shelfmark(capsys):
     full_title_i = "ASDFIB. 1345/"
     full_title_c = "ASDFC.145132/a"
     bad_title = "NOTHINGCAPTUREDHERE"
 
-    foundref_i = xmle.find_title_ref(full_title_i)
-    foundref_c = xmle.find_title_ref(full_title_c)
+    found_mark_i = xmle.find_title_shelfmark(full_title_i)
+    found_mark_c = xmle.find_title_shelfmark(full_title_c)
 
     # next one should print "Unrecognized title format" to stdout
-    foundref_bad = xmle.find_title_ref(bad_title)
+    _ = xmle.find_title_shelfmark(bad_title)
     captured = capsys.readouterr()
 
-    assert foundref_i == "IB. 1345."
-    assert foundref_c == "C.145132.a"
+    assert found_mark_i == "IB. 1345."
+    assert found_mark_c == "C.145132.a"
     assert captured.out == "Unrecognized title format\n"
 
 
-def test_gen_title_refs(lines, indices):
-    title_refs = xmle.gen_title_refs(lines, indices)
-
-    assert len(title_refs) == 2
-    assert title_refs == ["IA. 123TI", "IA. 789SE"]
+# def test_gen_title_refs(lines, indices):
+#     title_refs = xmle.gen_title_refs(lines, indices)
+#
+#     assert len(title_refs) == 2
+#     assert title_refs == ["IA. 123TI", "IA. 789SE"]
 
 
 def test_generate_xml():
@@ -171,32 +175,37 @@ def test_generate_xml():
 
 def test_extract_catalogue_entries(lines, indices):
     root = ET.parse("tests\\title_xml_example.xml").getroot()
-    _, xml_track_df = xmle.extract_lines_for_vol({"title_xml_example": root})
-    lines += ["", "IA. 666", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
-    indices += [[10, 11, 12], [14, 15, 16]]  # need an extra pair due to [:-2] indexing in save_raw_txt
-    title_refs = xmle.gen_title_refs(lines, indices)
+    lines, xml_track_df = xmle.extract_lines_for_vol({"title_xml_example": root})
+    title_shelfmarks, _ = xmle.find_headings(lines)
+    catalogue_entries = xmle.extract_catalogue_entries(lines, indices, title_shelfmarks, xml_track_df)
 
-    catalogue_entries = xmle.extract_catalogue_entries(lines, indices, title_refs, xml_track_df)
-
-    assert catalogue_entries.shape == (2,2)
-    assert catalogue_entries["labels"].tolist() == ["title_xml_example_IA_123", "title_xml_example_IA_789SE"]
+    assert catalogue_entries.shape == (2, 5)
+    assert catalogue_entries["xml"].tolist() == ["title_xml_example", "title_xml_example"]
+    assert catalogue_entries["shelfmark"].tolist() == ["IA. 789SE", "IA. 353"]
+    assert catalogue_entries["copy"].sum() == catalogue_entries.shape[0]
+    assert catalogue_entries["entry"].transform(len).tolist() == [6, 3]
+    assert catalogue_entries.loc[0, "entry"][0] == "TITLE"
+    assert catalogue_entries.loc[0, "entry"][-1] == "IA. 789"
+    assert catalogue_entries.loc[1, "entry"][0] == "SECONDTITLE"
+    assert catalogue_entries.loc[1, "entry"][-1] == "IA. 353"
 
 
 def test_save_raw_txt(tmp_path, lines, titles, indices):
     root = ET.parse("tests\\title_xml_example.xml").getroot()
     _, xml_track_df = xmle.extract_lines_for_vol({"title_xml_example": root})
     lines, indices = lines, indices
+    title_shelfmarks, _ = xmle.find_headings(lines)
+    title_shelfmarks += ["IA. 353"]
+    print(title_shelfmarks)
     lines += ["", "IA. 666", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
-    indices += [[10, 11, 12], [14, 15, 16]]  # need an extra pair due to [:-2] indexing in save_raw_txt
-    title_refs = xmle.gen_title_refs(lines, indices)
+    indices += [[10, 11, 12]]  # need an extra set due to [:-1] indexing in save_raw_txt
     out_path = tmp_path
 
-    xmle.save_raw_txt(lines, indices, title_refs, xml_track_df, out_path)
+    xmle.save_raw_txt(lines, indices, title_shelfmarks, xml_track_df, out_path)
 
     filepath_1 = tmp_path.joinpath(f"{xml_track_df.iloc[0, 0]}_IA_789SE.txt")
-    filepath_2 = tmp_path.joinpath(f"{xml_track_df.iloc[0,0]}_IA_666.txt")
+    filepath_2 = tmp_path.joinpath(f"{xml_track_df.iloc[0,0]}_IA_353.txt")
 
-    assert len(indices) == 4
     assert os.path.exists(filepath_1)
     assert os.path.exists(filepath_2)
 
