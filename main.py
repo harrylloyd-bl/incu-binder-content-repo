260091ba96ae482aecfbbe5d6be28433f1bb1049
harrylloyd-bl <harry.lloyd@bl.uk>
2024-01-23 11:28:03 +0000

    Improve progress logging and name clarity

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from src.data import xml_extraction as xmle
 
 if __name__ == '__main__':
 
-    for i in range(1, 3):
-        two_col_loc = f"data\\raw\\BMC_{i}\\BMC_{i}_2_column_model_output\\page"
-        four_col_loc = f"data\\raw\\BMC_{i}\\BMC_{i}_4_column_model_output\\page"
-        current_volume = xmle.gather_2_4_col_xmls(two_col_loc, four_col_loc)
+    for i in range(3, 4):
+        two_col_loc = f"data\\raw\\BMC_{i}_2\\*"
+        four_col_loc = f"data\\raw\\BMC_{i}_4\\*"
+        xmls_2, xmls_4 = xmle.gen_2_4_col_xml_paths(two_col_loc, four_col_loc)
+        print(f"{len(xmls_2) + len(xmls_4)} xmls extracted from\n"
+              f"2 col [{len(xmls_2):03}]: {os.path.dirname(xmls_2[0])}\n4 col [{len(xmls_4):03}]: {os.path.dirname(xmls_4[0])}")
+        vol_xml_trees = xmle.gen_2_4_col_xml_trees(xmls_2, xmls_4)
 
-        print("\nExtrating catalogue entries from xmls")
-        lines, xml_track_df = xmle.extract_lines_for_vol(current_volume)
+        print("\nExtracting catalogue entries from xmls")
+        lines, xml_track_df = xmle.extract_lines_for_vol(vol_xml_trees)
         title_shelfmarks, title_indices = xmle.find_headings(lines)
         entry_df = xmle.extract_catalogue_entries(lines, title_indices, title_shelfmarks, xml_track_df)
         print(f"Extracted {len(entry_df)} entries")
@@ -19,7 +22,7 @@ if __name__ == '__main__':
         if not os.path.exists(out_path):
             os.makedirs(out_path)
 
-        entry_df.to_csv(os.path.join(out_path, "catalogue_entries_leading_caps.csv"), index=False)
+        entry_df.to_csv(os.path.join(out_path, "catalogue_entries_sm_refactor.csv"), index=False)
 
         # xmle.save_poorly_scanned_pages(xmle.get_poorly_scanned_pages(current_volume, xmls), out_path)
         # print("Saving raw txt files")
